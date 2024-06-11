79375cb43f12d9e067169a3bfabb189988db4c3b
shahzad-ahmed <shahzadwarraich.sc@gmail.com>
2024-06-11 18:10:07 +0500

    feat: add sudoku solve logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,111 @@
 class SudokuSolver {
+  getRows(str) {
+    let rows = [];
+    let j = 0;
+
+    for (let i = 0; i < 9; i++) {
+      rows.push(str.slice(j, j = (i + 1) * 9).split(''));
+    }
+    return rows;
+  }
+
+  getCols(str) {
+    const arr = str.split('');
+    let i = 0;
+    let cols = [[], [], [], [], [], [], [], [], []]
+    while (i < 81) {
+      for (let j = 0; j < 9; j++) {
+        cols[j].push(arr[i])
+        i++;
+      }
+    }
+    return cols;
+  }
+
+  getRegions(str) {
+    const arr = this.getRows(str)
+    let grids = [[], [], [], [], [], [], [], [], []]
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        const gridIndex = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+        grids[gridIndex].push(arr[row][col]);
+      }
+    }
+    return grids;
+  }
 
   validate(puzzleString) {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    const arr = this.getRows(puzzleString);
+    // console.log(arr[row])
+    return (arr[row].includes(value.toString()));
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const arr = this.getCols(puzzleString)
 
+    return arr[column].includes(value.toString())
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const arr = this.getRegions(puzzleString)
+    const r = Math.floor(row / 3);
+    const c = Math.floor(column / 3);
+    const index = r * 3 + c;
+    // console.log(arr[index])
+    return arr[index].includes(value.toString());
+    // console.log(arr)
 
   }
 
-  solve(puzzleString) {
-    
+  checkPlacement(puzzle, row, col, val) {
+    if (this.checkRowPlacement(puzzle, row, col, val) || this.checkColPlacement(puzzle, row, col, val)
+      || this.checkRegionPlacement(puzzle, row, col, val)) {
+      return true;
+    }
+    return false
+  }
+  findEmptyBox(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] == '.') {
+          return [i, j]
+        }
+      }
+    }
+    return null
+  }
+
+  solve(board, puzzle) {
+    let slot = this.findEmptyBox(board)
+    const row = slot[0];
+    const column = slot[1]
+
+    if (!slot) {
+      console.log('backtracking done')
+      return board;
+    }
+
+    for (let num = 1; num <= 9; num++) {
+
+      if (this.checkPlacement(puzzle, row, column, num)) {
+        board[row][column] = num.toString();
+
+        if (this.solve(board)) {
+          return true;
+        }
+        
+        board[row][column] = '.'
+      }
+
+    }
+
+    return false;
+
   }
 }
+// console.log('---------' + i + '------')
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,74 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      // console.log(req.body)
+      const r = req.body;
+      const puzzle = r.puzzle;
+      const val = r.value;
+      let row = r.coordinate.split('')[0];
+      let col = r.coordinate.split('')[1];
+      col--;
+      row = getRowIndex(row.toUpperCase());
+      console.log('Row: ' + row + '   Col: ' + col)
+      // solver.checkRowPlacement(puzzle, row, col, val)
+      // console.log(solver.checkColPlacement(puzzle, row, col, val))
+      // solver.checkRegionPlacement(puzzle, row, col, val)
+      const ro = solver.checkRowPlacement(puzzle, row, col, val)
+      const co = solver.checkColPlacement(puzzle, row, col, val)
+      const re = solver.checkRegionPlacement(puzzle, row, col, val);
+      console.log({row: ro, column: co, region: re})
+      if(!ro && !co && !re) {
+        res.send(false)
+      }else res.send(true)
+      // res.send({row: ro, column: co, region: re})
 
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-
+      const puzzle = req.body.puzzle;
+      console.log(puzzle)
+      const board = solver.getRows(puzzle);
+      const solvedBoard = solver.solve(board, puzzle);
+      console.log(solvedBoard)
+      // solver.checkRegionPlacement(puzzle);
     });
 };
+
+function getRowIndex(row) {
+  switch (row) {
+    case 'A':
+      row = 0;
+      break;
+    case 'B':
+      row = 1;
+      break;
+    case 'C':
+      row = 2;
+      break;
+    case 'D':
+      row = 3;
+      break;
+    case 'E':
+      row = 4;
+      break;
+    case 'F':
+      row = 5;
+      break;
+    case 'G':
+      row = 6;
+      break;
+    case 'H':
+      row = 7;
+      break;
+    case 'I':
+      row = 8;
+      break;
+  }
+  return row;
+}
